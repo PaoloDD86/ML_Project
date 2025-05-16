0a9ad675e03e9b2006da1a95130644db097099af
Paolo_DD <p40l086@hotmail.it>
2025-05-16 17:14:23 +0200

    EDA and problem statesmen

--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,7 @@
-### Crea una cartella logs nella directory corrente se non esiste.
-### Genera un file .log con data e ora nel nome.
-### Configura il logging in modo da scrivere messaggi dettagliati lì dentro.
-### Se esegui lo script, scrive una riga nel log con un messaggio di test.
+# Crea una cartella logs nella directory corrente se non esiste.
+# Genera un file .log con data e ora nel nome.
+# Configura il logging in modo da scrivere messaggi dettagliati lì dentro.
+# Se esegui lo script, scrive una riga nel log con un messaggio di test.
 
 import logging
 import os
@@ -15,15 +15,15 @@ LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-    
-)
-### Salva i log nel file specificato.
-### Definisce il formato del messaggio di log.
-### Imposta il livello di log a INFO (cioè registra info, warning, errori, ecc.)
 
-### if __name__=="__main__":
-###    logging.info("Logging has started")   
-    
-### Serve per testare il logging se il file è eseguito direttamente, scrive nel log la riga Logging has started
+
+)
+# Salva i log nel file specificato.
+# Definisce il formato del messaggio di log.
+# Imposta il livello di log a INFO (cioè registra info, warning, errori, ecc.)
+
+# if __name__=="__main__":
+# logging.info("Logging has started")      
+# Serve per testare il logging se il file è eseguito direttamente, scrive nel log la riga Logging has started
