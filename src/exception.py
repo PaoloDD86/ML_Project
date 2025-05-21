3d4ab81d66fe64950d539ebafc2f881044b03679
Paolo_DD <p40l086@hotmail.it>
2025-05-21 11:27:28 +0200

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,14 +1,5 @@
-# Ha lo scopo di creare una gestione personalizzata degli errori in Python,
-# fornendo messaggi di errore dettagliati, incluso il nome del file, numero di riga e il messaggio dell’errore.
- 
 import sys
 from src.logger import logging
-
-# Configura il logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='[%(asctime)s] %(levelname)s - %(message)s'
-)
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -16,13 +7,7 @@ def error_message_detail(error,error_detail:sys):
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exc_tb.tb_lineno,str(error))
 
-    return error_message
-# Ottiene i dettagli dell’eccezione (in particolare la traccia dello stack exc_tb).
-# Estrae:
-# Il nome del file in cui si è verificato l’errore.
-# Il numero di riga.
-# Il messaggio di errore.
-# Compone un messaggio di errore dettagliato.
+    return error_message  
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
@@ -31,7 +16,3 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-
-# Cosa fa: è una classe di eccezione personalizzata che estende Exception.
-# Alla creazione, usa la funzione error_message_detail() per creare un messaggio d'errore dettagliato.
-# Ridefinisce __str__() per restituire questo messaggio personalizzato quando l'eccezione viene stampata.
--- src/logger.py
+++ src/logger.py
@@ -1,11 +1,11 @@
+import logging
+import os
+from datetime import datetime
+
 # Crea una cartella logs nella directory corrente se non esiste.
 # Genera un file .log con data e ora nel nome.
 # Configura il logging in modo da scrivere messaggi dettagliati lì dentro.
 # Se esegui lo script, scrive una riga nel log con un messaggio di test.
-
-import logging
-import os
-from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 log_path = os.path.join(os.getcwd(), "logs")
@@ -17,13 +17,13 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
-
 )
+
 # Salva i log nel file specificato.
 # Definisce il formato del messaggio di log.
 # Imposta il livello di log a INFO (cioè registra info, warning, errori, ecc.)
 
-# if __name__=="__main__":
-# logging.info("Logging has started")      
-# Serve per testare il logging se il file è eseguito direttamente, scrive nel log la riga Logging has started
+if __name__=="__main__":
+    logging.info ("Logging has started")      
+# Serve per testare il logging se il file è eseguito direttamente 
+# scrive nel log la riga Logging has started
